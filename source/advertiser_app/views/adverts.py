70afd160b1da994d86bd57d843fac941b3cc30fb
Gosha <begoshaaaa@gmail.com>
2021-08-21 17:19:37 +0600

    some styling is applied

--- source/advertiser_app/views/adverts.py
+++ source/advertiser_app/views/adverts.py
@@ -15,12 +15,13 @@ from advertiser_app.models import (
     Category
 )
 from django.views.generic.edit import FormMixin
-from constants.constants import DEFAULT_STATUS_ID
-from advertiser_app.forms import AdvertForm
+from advertiser_app.forms import AdvertForm, SimpleSearchForm
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 import json
+from django.db.models import Q
 from constants.constants import *
 from django.contrib import messages
+from django.utils.http import urlencode
 
 
 class AdvertList(ListView):
@@ -28,11 +29,34 @@ class AdvertList(ListView):
     context_object_name = 'adverts'
     model = Advert
     ordering = ['-created_at']
-    paginate_by = 10
+    paginate_by = 6
     paginate_orphans = 1
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+        return context
+
+    def get_search_form(self):
+        return SimpleSearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
 
     def get_queryset(self):
         queryset = super().get_queryset().filter(is_deleted__exact=False, status__exact=1)
+        if self.search_value:
+            query = Q(title__icontains=self.search_value) | Q(description__icontains=self.search_value)
+            queryset = queryset.filter(query)
         return queryset
 
 
@@ -42,6 +66,9 @@ class AdvertForModerationList(PermissionRequiredMixin, AdvertList):
 
     def get_queryset(self):
         queryset = Advert.objects.filter(status__exact=DEFAULT_STATUS_ID, is_deleted__exact=False)
+        if self.search_value:
+            query = Q(title__icontains=self.search_value) | Q(description__icontains=self.search_value)
+            queryset = queryset.filter(query)
         return queryset
 
 
