2e8bb9655f0941e00e6adc8253d120aca14ce591
Gosha <begoshaaaa@gmail.com>
2021-08-21 13:50:10 +0600

    path for delete view is added to url paths

--- source/advertiser_app/urls.py
+++ source/advertiser_app/urls.py
@@ -5,7 +5,8 @@ from advertiser_app.views.adverts import (
     AdvertDetail,
     AdvertDetailModerate,
     AdvertCreate,
-    AdvertUpdate
+    AdvertUpdate,
+    AdvertDelete
 )
 
 urlpatterns = [
@@ -15,4 +16,5 @@ urlpatterns = [
     path('advert/moderate/<int:pk>/', AdvertDetailModerate.as_view(), name='advert_detail_moderate'),
     path('create/', AdvertCreate.as_view(), name='create'),
     path('update/<int:pk>/', AdvertUpdate.as_view(), name='update'),
+    path('delete/<int:pk>/', AdvertDelete.as_view(), name='delete_advert')
 ]
