5a6e2f5bc211af70f81efcb98083e3334d50c068
Gosha <begoshaaaa@gmail.com>
2021-08-21 10:12:03 +0600

    models Advert, Status, Category are created && migrations is applied

--- source/advertiser/settings.py
+++ source/advertiser/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'phonenumber_field',
     # custom apps
     'accounts',
+    'advertiser_app'
 
 ]
 
@@ -128,6 +129,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
+MEDIA_URL = '/uploads/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- source/advertiser_app/models.py
+++ source/advertiser_app/models.py
@@ -1,3 +1,102 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Advert(models.Model):
+    title = models.CharField(
+        max_length=200,
+        null=False,
+        blank=False,
+        verbose_name='Title'
+    )
+    description = models.TextField(
+        max_length=2000,
+        null=False,
+        blank=False,
+        verbose_name='Description'
+    )
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        null=False,
+        related_name='adverts',
+        verbose_name='Author'
+    )
+    image = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to='images',
+        verbose_name='Image'
+    )
+    price = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        verbose_name='Price'
+    )
+    category = models.ForeignKey(
+        'advertiser_app.Category',
+        on_delete=models.DO_NOTHING,
+        related_name='adverts',
+        verbose_name='Category'
+    )
+    status = models.ForeignKey(
+        'advertiser_app.Status',
+        on_delete=models.DO_NOTHING,
+        related_name='adverts',
+        verbose_name='Status'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Created At'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Updated At'
+    )
+    published_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Published At'
+    )
+
+    class Meta:
+        db_table = 'adverts'
+        verbose_name = 'Advert'
+        verbose_name_plural = 'Adverts'
+
+    def __str__(self):
+        return f'{self.title} - {self.author}'
+
+
+class Status(models.Model):
+    status = models.CharField(
+        max_length=20,
+        null=False,
+        blank=False,
+        verbose_name='Status'
+    )
+
+    class Meta:
+        db_table = 'statuses'
+        verbose_name = 'Status'
+        verbose_name_plural = 'Statuses'
+
+    def __str__(self):
+        return self.status
+
+
+class Category(models.Model):
+    category = models.CharField(
+        max_length=20,
+        null=False,
+        blank=False,
+        verbose_name='Category'
+    )
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.category
