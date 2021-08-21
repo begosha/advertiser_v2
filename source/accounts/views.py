50287380f5a70a5704d61f376bec455332e7052f
Gosha <begoshaaaa@gmail.com>
2021-08-21 09:52:02 +0600

    user auth & edit views are added

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,3 +1,103 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout, get_user_model, update_session_auth_hash
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from .forms import MyUserCreationForm, UserChangeForm, ProfileChangeForm, PasswordChangeForm
+from webapp.forms import SimpleSearchForm
+from django.views.generic import DetailView, ListView, UpdateView, CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.utils.http import urlencode
+from accounts.models import Profile
 
-# Create your views here.
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+    paginate_related_by = 5
+    paginate_related_orphans = 0
+
+
+class RegisterView(CreateView):
+    model = User
+    template_name = 'user_create.html'
+    form_class = MyUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('index')
+        return next_url
+
+
+class UserChangeView(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = UserChangeForm
+    template_name = 'user_change.html'
+    context_object_name = 'user_obj'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_context_data(self, **kwargs):
+        if 'profile_form' not in kwargs:
+            kwargs['profile_form'] = self.get_profile_form()
+        return super().get_context_data(**kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        profile_form = self.get_profile_form()
+        if form.is_valid() and profile_form.is_valid():
+            return self.form_valid(form, profile_form)
+        else:
+            return self.form_invalid(form, profile_form)
+
+    def form_valid(self, form, profile_form):
+        response = super().form_valid(form)
+        profile_form.save()
+        return response
+
+    def form_invalid(self, form, profile_form):
+        context = self.get_context_data(form=form, profile_form=profile_form)
+        return self.render_to_response(context)
+
+    def get_profile_form(self):
+        form_kwargs = {'instance': self.object.profile}
+        if self.request.method == 'POST':
+            form_kwargs['data'] = self.request.POST
+            form_kwargs['files'] = self.request.FILES
+        return ProfileChangeForm(**form_kwargs)
+
+    def get_success_url(self):
+        return reverse('detail', kwargs={'pk': self.object.pk})
+
+
+class UserPasswordChangeView(UpdateView):
+    model = get_user_model()
+    template_name = 'user_password_change.html'
+    form_class = PasswordChangeForm
+    context_object_name = 'user_obj'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def form_valid(self, form):
+        response = super(UserPasswordChangeView, self).form_valid(form)
+        update_session_auth_hash(self.request, user)
+        return response
+
+    def get_success_url(self):
+        return reverse('index')
+
+
+
+
