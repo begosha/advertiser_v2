9dd38c741996f61b69d9885c49c14bec3f7f2f40
Gosha <begoshaaaa@gmail.com>
2021-08-21 12:17:43 +0600

    moderator views are separated

--- source/advertiser_app/urls.py
+++ source/advertiser_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from advertiser_app.views.adverts import (
-    AdvertList
+    AdvertList,
+    AdvertForModerationList,
+    AdvertDetail,
+    AdvertDetailModerate
 )
 
 urlpatterns = [
     path('', AdvertList.as_view(), name='advert_list'),
+    path('moderation/', AdvertForModerationList.as_view(), name='moderation'),
+    path('advert/<int:pk>/', AdvertDetail.as_view(), name='advert_detail'),
+    path('advert/moderate/<int:pk>/', AdvertDetailModerate.as_view(), name='advert_detail_moderate'),
 ]
